e85eb42f9e77e7d0eb49005f873034dce16595c2
Reuben Ellis <ethriel3695@gmail.com>
2022-03-05 07:29:01 -0700

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}) {
   return (
     <div className={`list-item ${state}`}>
       <label className="checkbox">
@@ -18,15 +22,23 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           aria-label={`archiveTask-${id}`}
         />
       </label>
-      <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
-      </div>
 
-      <div className="actions" onClick={event => event.stopPropagation()}>
+      <input
+        type="text"
+        value={title}
+        readOnly={true}
+        placeholder="Input title"
+        style={{ background: 'red' }}
+      />
+      <div className="actions" onClick={(event) => event.stopPropagation()}>
         {state !== 'TASK_ARCHIVED' && (
           // eslint-disable-next-line jsx-a11y/anchor-is-valid
           <a onClick={() => onPinTask(id)}>
-            <span className={`icon-star`} id={`pinTask-${id}`} aria-label={`pinTask-${id}`} />
+            <span
+              className={`icon-star`}
+              id={`pinTask-${id}`}
+              aria-label={`pinTask-${id}`}
+            />
           </a>
         )}
       </div>
@@ -35,17 +47,17 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
 }
 
 Task.propTypes = {
- /** Composition of the task */
- task: PropTypes.shape({
-   /** Id of the task */
-   id: PropTypes.string.isRequired,
-   /** Title of the task */
-   title: PropTypes.string.isRequired,
-   /** Current state of the task */
-   state: PropTypes.string.isRequired,
- }),
- /** Event to change the task to archived */
- onArchiveTask: PropTypes.func,
- /** Event to change the task to pinned */
- onPinTask: PropTypes.func,
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
 };
